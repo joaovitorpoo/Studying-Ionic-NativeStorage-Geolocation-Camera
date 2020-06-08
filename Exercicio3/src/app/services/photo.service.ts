33ab969787e4654de33604024e8e75bf1ad79a36
João Vitor <63071327+joaovitorpoo@users.noreply.github.com>
2020-06-07 22:50:13 -0300

    Galeria de Fotos Tab 3

---
+++ Exercicio3/src/app/services/photo.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { Plugins, CameraResultType, Capacitor, FilesystemDirectory, 
+  CameraPhoto, CameraSource } from '@capacitor/core';
+import { Photo } from '../model/photo.model';
+const { Camera, Filesystem, Storage } = Plugins;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+  
+  public photos: Photo[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri, 
+      source: CameraSource.Camera, 
+      quality: 100 
+    });
+
+    this.photos.unshift({
+      filepath: "soon...",
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+}
